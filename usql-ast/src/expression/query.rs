4e167539d37e528ccb9d22d77a3d149a85123944
Qinxuan Chen <koushiro.cqx@gmail.com>
2021-11-26 18:10:54 +0800

    usql-ast: improve query expression (#18)
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- usql-ast/src/expression/query.rs
+++ usql-ast/src/expression/query.rs
@@ -13,8 +13,8 @@ pub struct Query {
     pub with: Option<With>,
     /// SELECT or UNION / EXCEPT / INTERSECT
     pub body: QueryBody,
-    /// `ORDER BY { <expr> [ ASC | DESC ] [ NULLS { FIRST | LAST } ] } [, ...]`
-    pub order_by: Vec<OrderBy>,
+    /// `ORDER BY { <sort_key> [ ASC | DESC ] [ NULLS FIRST | NULLS LAST ] } [, ...]`
+    pub order_by: Option<OrderBy>,
     /// `LIMIT { <N> | ALL }`
     pub limit: Option<Limit>,
     /// `OFFSET <N> [ { ROW | ROWS } ]`
@@ -29,8 +29,8 @@ impl fmt::Display for Query {
             write!(f, "{} ", with)?;
         }
         write!(f, "{}", self.body)?;
-        if !self.order_by.is_empty() {
-            write!(f, " ORDER BY {}", display_comma_separated(&self.order_by))?;
+        if let Some(order_by) = &self.order_by {
+            write!(f, " {}", order_by)?;
         }
         if let Some(limit) = &self.limit {
             write!(f, " {}", limit)?;
@@ -111,272 +111,9 @@ impl fmt::Display for SetQuantifier {
     }
 }
 
-/// A restricted variant of `SELECT` (without CTEs/`ORDER BY`), which may
-/// appear either as the only body item of an `Query`, or as an operand to a
-/// set operation like `UNION`.
-#[derive(Clone, Debug, Eq, PartialEq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct Select {
-    /// Set quantifier
-    pub quantifier: Option<SetQuantifier>,
-    /// projection expressions
-    pub projection: Vec<SelectItem>,
-
-    /// FROM clause
-    pub from: Vec<TableWithJoins>,
-    /// WHERE clause
-    pub selection: Option<Box<Expr>>,
-    /// GROUP BY clause
-    pub group_by: Vec<Expr>,
-    /// HAVING clause
-    pub having: Option<Box<Expr>>,
-    /// WINDOW clause
-    pub windows: Option<Vec<Window>>,
-}
-
-impl fmt::Display for Select {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_str("SELECT")?;
-        if let Some(quantifier) = &self.quantifier {
-            write!(f, " {}", quantifier)?;
-        }
-        write!(f, " {}", display_comma_separated(&self.projection))?;
-        if !self.from.is_empty() {
-            write!(f, " FROM {}", display_comma_separated(&self.from))?;
-        }
-        if let Some(selection) = &self.selection {
-            write!(f, " WHERE {}", selection)?;
-        }
-        if !self.group_by.is_empty() {
-            write!(f, " GROUP BY {}", display_comma_separated(&self.group_by))?;
-        }
-        if let Some(having) = &self.having {
-            write!(f, " HAVING {}", having)?;
-        }
-        if let Some(windows) = &self.windows {
-            write!(f, " WINDOW {}", display_comma_separated(windows))?;
-        }
-        Ok(())
-    }
-}
-
-/// One item of the comma-separated list following `SELECT`
-#[derive(Clone, Debug, Eq, PartialEq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub enum SelectItem {
-    /// An unqualified `*`
-    Wildcard,
-    /// `alias.*` or even `schema.table.*`
-    QualifiedWildcard(ObjectName),
-    /// An expression, maybe followed by `[ AS ] alias`
-    #[doc(hidden)]
-    DerivedColumn {
-        expr: Box<Expr>,
-        alias: Option<Ident>,
-    },
-}
-
-impl fmt::Display for SelectItem {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            SelectItem::Wildcard => write!(f, "*"),
-            SelectItem::QualifiedWildcard(prefix) => write!(f, "{}.*", prefix),
-            SelectItem::DerivedColumn { expr, alias } => {
-                if let Some(alias) = alias {
-                    write!(f, "{} AS {}", expr, alias)
-                } else {
-                    write!(f, "{}", expr)
-                }
-            }
-        }
-    }
-}
-
-/// From clause.
-#[doc(hidden)]
-#[derive(Clone, Debug, Eq, PartialEq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct TableWithJoins {
-    pub relation: TableFactor,
-    pub joins: Vec<Join>,
-}
-
-impl fmt::Display for TableWithJoins {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.relation)?;
-        for join in &self.joins {
-            write!(f, " {}", join)?;
-        }
-        Ok(())
-    }
-}
-
-/// A table name or a parenthesized subquery with an optional alias
-#[doc(hidden)]
-#[derive(Clone, Debug, Eq, PartialEq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub enum TableFactor {
-    Table {
-        /// Table or query name.
-        name: ObjectName,
-        alias: Option<TableAlias>,
-    },
-    Derived {
-        lateral: bool,
-        subquery: Box<Query>,
-        alias: Option<TableAlias>,
-    },
-    /// Represents a parenthesized joined table.
-    /// The SQL spec only allows a join expression
-    /// (`(foo <JOIN> bar [ <JOIN> baz ... ])`) to be nested, possibly several times.
-    NestedJoin(Box<TableWithJoins>),
-}
-
-impl fmt::Display for TableFactor {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::Table { name, alias } => {
-                write!(f, "{}", name)?;
-                if let Some(alias) = alias {
-                    write!(f, " AS {}", alias)?;
-                }
-                Ok(())
-            }
-            Self::Derived {
-                lateral,
-                subquery,
-                alias,
-            } => {
-                if *lateral {
-                    write!(f, "LATERAL ")?;
-                }
-                write!(f, "({})", subquery)?;
-                if let Some(alias) = alias {
-                    write!(f, " AS {}", alias)?;
-                }
-                Ok(())
-            }
-            Self::NestedJoin(table) => write!(f, "({})", table),
-        }
-    }
-}
-
-/// Table alias.
-#[derive(Clone, Debug, Eq, PartialEq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct TableAlias {
-    /// Alias name.
-    pub name: Ident,
-    /// Columns.
-    pub columns: Vec<Ident>,
-}
-
-impl fmt::Display for TableAlias {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.name)?;
-        if !self.columns.is_empty() {
-            write!(f, " ({})", display_comma_separated(&self.columns))?;
-        }
-        Ok(())
-    }
-}
-
-/// The `JOIN` relation.
-#[doc(hidden)]
-#[derive(Clone, Debug, Eq, PartialEq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct Join {
-    pub join: JoinOperator,
-    pub relation: TableFactor,
-}
-
-impl fmt::Display for Join {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match &self.join {
-            JoinOperator::CrossJoin => write!(f, "CROSS JOIN {}", self.relation),
-            JoinOperator::Inner(constraint) => write!(
-                f,
-                "{}JOIN {}{}",
-                constraint.prefix(),
-                self.relation,
-                constraint.suffix(),
-            ),
-            JoinOperator::LeftOuter(constraint) => write!(
-                f,
-                "{}LEFT JOIN {}{}",
-                constraint.prefix(),
-                self.relation,
-                constraint.suffix(),
-            ),
-            JoinOperator::RightOuter(constraint) => write!(
-                f,
-                "{}RIGHT JOIN {}{}",
-                constraint.prefix(),
-                self.relation,
-                constraint.suffix(),
-            ),
-            JoinOperator::FullOuter(constraint) => write!(
-                f,
-                "{}FULL JOIN {}{}",
-                constraint.prefix(),
-                self.relation,
-                constraint.suffix(),
-            ),
-        }
-    }
-}
-
-/// The join operator.
-#[doc(hidden)]
-#[derive(Clone, Debug, Eq, PartialEq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub enum JoinOperator {
-    CrossJoin,
-    Inner(JoinConstraint),
-    LeftOuter(JoinConstraint),
-    RightOuter(JoinConstraint),
-    FullOuter(JoinConstraint),
-}
-
-/// The constraint of join operator.
-#[doc(hidden)]
-#[derive(Clone, Debug, Eq, PartialEq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub enum JoinConstraint {
-    On(Expr),
-    Using(Vec<Ident>),
-    Natural,
-    None,
-}
-
-impl JoinConstraint {
-    fn prefix(&self) -> &'static str {
-        match self {
-            Self::Natural => "NATURAL ",
-            _ => "",
-        }
-    }
-
-    fn suffix(&self) -> impl fmt::Display + '_ {
-        struct Suffix<'a>(&'a JoinConstraint);
-        impl<'a> fmt::Display for Suffix<'a> {
-            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                match self.0 {
-                    JoinConstraint::On(expr) => write!(f, " ON {}", expr),
-                    JoinConstraint::Using(attrs) => {
-                        write!(f, " USING({})", display_comma_separated(attrs))
-                    }
-                    _ => Ok(()),
-                }
-            }
-        }
-        Suffix(self)
-    }
-}
-
-// ================================================================================================
-// Optional clause
-// ================================================================================================
+// ============================================================================
+// with clause
+// ============================================================================
 
 /// With clause.
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
@@ -421,15 +158,37 @@ impl fmt::Display for Cte {
     }
 }
 
+// ============================================================================
+// order by clause
+// ============================================================================
+
 /// `ORDER BY` clause.
 ///
 /// ```txt
-/// <expr> [ASC | DESC] [NULLS FIRST | NULLS LAST]
+/// ORDER BY <sort specification>  [ { ,  <sort specification>  }... ]
 /// ```
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct OrderBy {
-    /// Order by expression
+    /// The sort specification list.
+    pub list: Vec<SortSpec>,
+}
+
+impl fmt::Display for OrderBy {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "ORDER BY {}", display_comma_separated(&self.list))
+    }
+}
+
+/// A sort specification.
+///
+/// ```txt
+/// <sort_key>  [ ASC | DESC  ] [ NULLS FIRST | NULLS LAST  ]
+/// ```
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct SortSpec {
+    /// Sort key
     pub expr: Box<Expr>,
     /// Optional `ASC` or `DESC`
     pub asc: Option<bool>,
@@ -437,7 +196,7 @@ pub struct OrderBy {
     pub nulls_first: Option<bool>,
 }
 
-impl fmt::Display for OrderBy {
+impl fmt::Display for SortSpec {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.expr)?;
         match self.asc {
@@ -454,7 +213,11 @@ impl fmt::Display for OrderBy {
     }
 }
 
-/// Limit clause (Not-standard).
+// ============================================================================
+// limit clause (Not ANSI SQL standard, but most dialects support it)
+// ============================================================================
+
+/// Limit clause.
 ///
 /// ```txt
 /// LIMIT [ offset, ] row_count
@@ -476,6 +239,10 @@ impl fmt::Display for Limit {
         }
     }
 }
+
+// ============================================================================
+// result offset clause
+// ============================================================================
 
 /// Offset clause.
 ///
@@ -517,6 +284,10 @@ impl fmt::Display for OffsetRows {
     }
 }
 
+// ============================================================================
+// fetch first clause
+// ============================================================================
+
 /// Fetch first clause.
 ///
 /// ```txt
@@ -541,22 +312,5 @@ impl fmt::Display for Fetch {
         } else {
             write!(f, "FETCH FIRST ROWS {}", extension)
         }
-    }
-}
-
-/// Window clause.
-#[doc(hidden)]
-#[derive(Clone, Debug, Eq, PartialEq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct Window {
-    /// New window name.
-    pub name: Ident,
-    /// Window specification.
-    pub spec: WindowSpec,
-}
-
-impl fmt::Display for Window {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} AS ({})", self.name, self.spec)
     }
 }
